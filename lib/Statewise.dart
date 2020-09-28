555c9cbbe1e3cdbde0baa89fffe02d5f580ef217
Karlsdevil9 <karlsdevil4@gmail.com>
2020-09-29 03:21:37 +0530

    Organised ListView

--- lib/Statewise.dart
+++ lib/Statewise.dart
@@ -30,19 +30,60 @@ class _StatewiseCasesState extends State<StatewiseCases> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: cases == null ? 0 : (cases.length - 1),
-        itemBuilder: (context, index) {
-          if (cases[index + 1]["state"] == "State Unassigned") {
-          } else {
-            return new Row(
-              children: [
-                Text(cases[index + 1]["state"]),
-                Text(cases[index + 1]["confirmed"])
-              ],
-            );
-          }
-        },
+      appBar: AppBar(
+        title: Text("CovidUpdates"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 70,
+            child: Card(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "StateName",
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    "TotalCases",
+                    textAlign: TextAlign.end,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Flexible(
+            child: ListView.builder(
+              itemCount: cases == null ? 0 : (cases.length - 1),
+              itemBuilder: (context, index) {
+                if (cases[index + 1]["state"] == "State Unassigned") {
+                } else {
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                    height: 70,
+                    child: new Card(
+                      elevation: 5,
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            cases[index + 1]["state"],
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            cases[index + 1]["confirmed"],
+                            textAlign: TextAlign.right,
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
